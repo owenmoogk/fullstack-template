49d66425a45755d363769ad346e6839ae951479b
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2021-10-14 18:35:54 -0400

    if a user already exists we need to return that :secret:
    when i migrated from serilizers i forgot about this :/

--- src/users/views.py
+++ src/users/views.py
@@ -20,6 +20,8 @@ class Signup(APIView):
         # CREATING USER
         if not request.data['username'] or not request.data['password']:
             return Response({'error': 'Field may not be empty'}, status=status.HTTP_400_BAD_REQUEST)
+        if User.objects.filter(username = request.data['username']).exists():
+            return Response({'Error': "A user with this username already exists"},status=status.HTTP_400_BAD_REQUEST)
         new_user = User(username = request.data['username'])
         new_user.set_password(request.data['password'])
         new_user.save()
